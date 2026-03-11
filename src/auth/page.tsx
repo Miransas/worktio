f68b3a7c5641d86d37f506140c8c94b500297913
sardorazimov <sardorazimov2901@gmail.com>
2026-03-11 05:09:34 +0300

    bug auth

--- src/auth/page.tsx
+++ src/auth/page.tsx
@@ -1,25 +1,9 @@
-import { invoke } from '@tauri-apps/api/core';
-import React, { useState } from 'react'
+import React from 'react'
 
 const page = () => {
-    const [youtubeKey, setYoutubeKey] = useState('');
-    const [telegramToken, setTelegramToken] = useState('');
-    const [geminiKey, setGeminiKey] = useState('');
-
-    const saveKeys = async () => {
-        try {
-            await invoke('save_api_key', { name: 'YT_KEY', value: youtubeKey });
-            await invoke('save_api_key', { name: 'TG_TOKEN', value: telegramToken });
-            await invoke('save_api_key', { name: 'GEMINI_KEY', value: geminiKey });
-            // Başarılıysa Dashboard'a yönlendir
-        } catch (err) {
-            console.error("Ustam anahtarlar kasaya girmedi!", err);
-        }
-    };
-
-    return (
-        <div>page</div>
-    )
+  return (
+    <div>page</div>
+  )
 }
 
 export default page
